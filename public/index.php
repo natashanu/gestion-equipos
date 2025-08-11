845b9c90329076de980ca63130912b82f1e31d52
Natasha Núñez <natashanu6@gmail.com>
2025-08-11 08:56:49 +0200

    fix: desactivar visualización de errores en consola y pantalla

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-require_once __DIR__.'/../app/lib/Core.php';
-require_once __DIR__.'/../app/config/config.php';
+require_once __DIR__ . '/../app/lib/Core.php';
+require_once __DIR__ . '/../app/config/config.php';
 
 $init = new Core();
-?>
